d33b01c7ac56a0454a3b5a6c9d5a176ccf09527a
naythankik <dolapokiki3ird@gmail.com>
2022-11-08 07:52:07 +0100

    working perfectly

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let first = require("./utilities/first");
+let { sum, concat } = require("./utilities/second");
 
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
+const arr2 = [20, 17, 34, 10, 9, 8];
+
+// console.log(first.cut3(arr1));
+// console.log(first.largeNumber(arr)2);
+
+// console.log(sum(arr1));
+// console.log(concat(arr1, arr2));
